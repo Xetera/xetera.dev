57979527609cf4cd138c45a74701109401e5f527
Xetera <mythologyali@gmail.com>
2020-07-10 16:06:09 -0700

    added netlify caching

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,6 +98,7 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    "gatsby-plugin-netlify-cache",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
