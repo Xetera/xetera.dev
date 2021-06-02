da5951ef198acf905c6f92079c2a44600e27da46
Xetera <mythologyali@gmail.com>
2021-06-03 02:48:52 +0300

    fix fonts?

--- fix-fonts.sh
+++ fix-fonts.sh
@@ -8,11 +8,11 @@ apt-get install fonts-noto-color-emoji -y
 curl -L https://noto-website-2.storage.googleapis.com/pkgs/NotoColorEmoji-unhinted.zip -o NotoColorEmoji.zip
 unzip NotoColorEmoji.zip
 
-mkdir "~/.fonts"
+mkdir "$HOME/.fonts"
 
-mv NotoColorEmoji.ttf "~/.fonts"
+mv NotoColorEmoji.ttf "$HOME/.fonts"
 
-cat << EOF > ~/.fonts.conf
+cat << EOF > "$HOME/.fonts.conf"
 <?xml version="1.0"?>
 <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
 <fontconfig>
