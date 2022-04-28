433eea5424efaac2ac5ae35207531f39651593b9
Xetera <mythologyali@gmail.com>
2022-04-28 22:03:33 +0300

    fixed lanyard reconnections

--- src/hooks/lanyard.jsx
+++ src/hooks/lanyard.jsx
@@ -1,10 +1,50 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
+
+const MAX_FAILED_CONNECTIONS = 5
 
 export function useLanyard(id) {
   const discordId = id
   const heartBeatInterval = useRef()
   const socket = useRef()
+  const reconnectAttempts = useRef()
   const [data, setData] = useState({})
+
+  const attemptConnect = () => {
+    const ws = new WebSocket("wss://api.lanyard.rest/socket")
+    ws.addEventListener("close", attemptReconnect)
+    ws.addEventListener("message", messageListener)
+    ws.addEventListener("open", open)
+    return ws
+  }
+
+  const open = () => {
+    console.log("Connected to lanyard")
+    reconnectAttempts.current = 0
+  }
+
+  const attemptReconnect = useCallback(() => {
+    if (reconnectAttempts.current > MAX_FAILED_CONNECTIONS) {
+      console.warn(
+        `Could not establish connection after ${MAX_FAILED_CONNECTIONS} attempts`
+      )
+      return
+    }
+    console.log("Attempting to reconnect to lanyard")
+    reconnectAttempts.current++
+    setTimeout(() => {
+      socket.current = attemptConnect()
+    }, 200)
+  }, [])
+
+  const cleanup = socket => {
+    if (heartBeatInterval.current) {
+      clearInterval(heartBeatInterval.current)
+    }
+    socket.removeEventListener("message", messageListener)
+    socket.removeEventListener("close", attemptReconnect)
+    socket.removeEventListener("open", open)
+  }
+
   const messageListener = useCallback(message => {
     const incoming = JSON.parse(message.data)
     if (incoming.op === 1) {
@@ -20,15 +60,10 @@ export function useLanyard(id) {
     }
     setData(incoming.d)
   }, [])
+
   useEffect(() => {
-    socket.current = new WebSocket("wss://api.lanyard.rest/socket")
-    socket.current.addEventListener("message", messageListener)
-    return () => {
-      if (heartBeatInterval.current) {
-        clearInterval(heartBeatInterval.current)
-      }
-      socket.current.removeEventListener("message", messageListener)
-    }
+    socket.current = attemptConnect()
+    return () => cleanup(socket.current)
   }, [])
   return {
     ...data,
