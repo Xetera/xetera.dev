0641fbcbf454e799b0a58c3bdf94da6bbcaa40e1
Xetera <contact@xetera.dev>
2023-03-12 19:24:11 +0300

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
-import 'dotenv/config';
-import { defineConfig } from 'astro/config';
-import unocss from './uno.config.js';
+import "dotenv/config";
+import { defineConfig } from "astro/config";
+import unocss from "./uno.config.js";
 import mdx from "@astrojs/mdx";
-import { remarkReadingTime } from './reading-time.mjs';
+import { remarkReadingTime } from "./reading-time.mjs";
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
-import rehypeExternalLinks from 'rehype-external-links';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
-import m2dx from "astro-m2dx"
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import m2dx from "astro-m2dx";
 
 /** @type {import('astro-m2dx').Options} */
 const m2dxOptions = {
@@ -20,29 +20,42 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: { assets: true },
   site: process.env.SITE_URL,
   markdown: {
-    remarkPlugins: [[m2dx, m2dxOptions], remarkReadingTime, rehypeSlug, rehypeAutolinkHeadings, rehypeExternalLinks],
+    remarkPlugins: [
+      [m2dx, m2dxOptions],
+      remarkReadingTime,
+      rehypeSlug,
+      rehypeAutolinkHeadings,
+      rehypeExternalLinks,
+    ],
     extendDefaultPlugins: true,
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'css-variables',
+      theme: "css-variables",
       // theme: 'min-light',
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ['ts', 'js', 'haskell', 'rust'],
+      langs: ["ts", "js", "haskell", "rust"],
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true
-    }
+      wrap: true,
+    },
   },
-  integrations: [unocss, mdx(), image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-    cacheDir: '.sharp'
-  }), prefetch({
-    throttle: 3
-  }), react()],
+  integrations: [
+    unocss,
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+      cacheDir: ".sharp",
+    }),
+    prefetch({
+      throttle: 3,
+    }),
+    react(),
+  ],
   output: "static",
   // adapter: cloudflare()
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,34 +1,33 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z, image } from "astro:content";
 // 2. Define your collection(s)
 
 const blogSchema = z.object({
-	title: z.string(),
-	date: z.coerce.date(),
-	description: z.string(),
-	tags: z.array(z.string()),
-	draft: z.boolean().default(false),
-	banner: z
-		.object({
-			url: z.string(),
-			aspectRatio: z.string().regex(/\d+:\d+/),
-		})
-		.optional(),
-	imageTop: z
-		.object({
-			src: z.string(),
-		})
-		.optional(),
+  title: z.string(),
+  date: z.coerce.date(),
+  description: z.string(),
+  tags: z.array(z.string()),
+  draft: z.boolean().default(false),
+  banner: image(),
+  // ({ url: z.string(),
+  // 	aspectRatio: z.string().regex(/\d+:\d+/),
+  // })
+  // .optional(),
+  imageTop: z
+    .object({
+      src: z.string(),
+    })
+    .optional(),
 });
 
 export type BlogSchema = z.infer<typeof blogSchema>;
 
 const blogCollection = defineCollection({
-	schema: blogSchema,
-	/* ... */
+  schema: blogSchema,
+  /* ... */
 });
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-	blog: blogCollection,
+  blog: blogCollection,
 };
