c2fef141d226a18844ce6f2dfc955f62da908084
Xetera <contact@xetera.dev>
2026-06-19 12:12:03 +0300

    h2 in homepage section

--- src/components/section-header.tsx
+++ src/components/section-header.tsx
@@ -2,21 +2,21 @@ import cls from "clsx";
 import type { PropsWithChildren } from "react";
 
 interface Props extends PropsWithChildren<{}> {
-	className?: string;
+  className?: string;
 }
 
 export default function SectionHeader(props: Props) {
-	return (
-		<div
-			className={cls(
-				props.className,
-				"flex justify-between items-center flex-wrap gap-2",
-			)}
-		>
-			<h1 className="text-lg color-text-500 font-semibold flex gap-2 items-center">
-				{props.children}
-			</h1>
-			<slot name="aside" />
-		</div>
-	);
+  return (
+    <div
+      className={cls(
+        props.className,
+        "flex justify-between items-center flex-wrap gap-2",
+      )}
+    >
+      <h2 className="text-lg color-text-500 font-semibold flex gap-2 items-center">
+        {props.children}
+      </h2>
+      <slot name="aside" />
+    </div>
+  );
 }
